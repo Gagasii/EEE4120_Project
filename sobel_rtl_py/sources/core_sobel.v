ff7d6787276f6557540d78225b02ddb2f742cc87
Gagasii <88988422+Gagasii@users.noreply.github.com>
2023-05-27 02:54:24 +0200

    Update core_sobel.v

--- sobel_rtl_py/sources/core_sobel.v
+++ sobel_rtl_py/sources/core_sobel.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
-module core_sobel ( p0, p1, p2, p3, p5, p6, p7, p8, out);
+module core_sobel ( p0, p1, p2, p3, p4, p5, p6, p7, out);
 
-input  [7:0] p0,p1,p2,p3,p5,p6,p7,p8;	// 8 bit pixels inputs
+  input  [7:0] p0,p1,p2,p3,p5,p6,p7,p4;	// 8 bit pixels inputs
 output [7:0] out;					// 8 bit output pixel
 
 wire signed [10:0] gx,gy;    //11 bits because max value of gx and gy is 
@@ -9,8 +9,8 @@ wire signed [10:0] gx,gy;    //11 bits because max value of gx and gy is
 wire signed [10:0] abs_gx,abs_gy;	//it is used to find the absolute value of gx and gy
 wire [10:0] sum;			//the max value is 255*8. here no sign bit needed.
 
-assign gx=((p2-p0)+((p5-p3)<<1)+(p8-p6));//sobel mask for gradient in horiz. direction
-assign gy=((p0-p6)+((p1-p7)<<1)+(p2-p8));//sobel mask for gradient in vertical direction
+  assign gx=((p2-p0)+((p4-p3)<<1)+(p7-p5));//sobel mask for gradient in horiz. direction
+  assign gy=((p0-p5)+((p1-p6)<<1)+(p2-p7));//sobel mask for gradient in vertical direction
 
 assign abs_gx = (gx[10]? ~gx+1 : gx);	// to find the absolute value of gx.
 assign abs_gy = (gy[10]? ~gy+1 : gy);	// to find the absolute value of gy. / bitwise negation +1 is 2's comp. of value.
